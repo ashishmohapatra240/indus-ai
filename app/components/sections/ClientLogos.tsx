2d85cad3ba66552fee083e1b488988abae05417b
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-04-01 17:25:50 +0530

    Refactor ClientLogos component to display images instead of text names, update section heading, and comment out Marquee

--- app/components/sections/ClientLogos.tsx
+++ app/components/sections/ClientLogos.tsx
@@ -1,26 +1,32 @@
-import Container from '../ui/Container'
-import Marquee from 'react-fast-marquee'
+import Container from "../ui/Container";
+import Image from "next/image";
+// import Marquee from "react-fast-marquee";
+
+// const clients = [
+//   { name: "Microsoft" },
+//   { name: "Google" },
+//   { name: "Amazon" },
+//   { name: "Meta" },
+//   { name: "Apple" },
+//   { name: "Tesla" },
+//   { name: "IBM" },
+//   { name: "Oracle" },
+// ];
 
 const clients = [
-  { name: 'Microsoft' },
-  { name: 'Google' },
-  { name: 'Amazon' },
-  { name: 'Meta' },
-  { name: 'Apple' },
-  { name: 'Tesla' },
-  { name: 'IBM' },
-  { name: 'Oracle' },
-]
+  { image: "/images/support/navrachna.png" },
+  { image: "/images/support/startupindia.png" },
+];
 
 export default function ClientLogos() {
   return (
     <section className="py-24 overflow-hidden bg-white">
       <Container>
         <h2 className="text-center text-sm font-medium text-gray-500 mb-16 font-raleway">
-          Trusted by world-class companies
+          Supported by
         </h2>
       </Container>
-      <Marquee
+      {/* <Marquee
         gradient={true}
         gradientColor="#F9FAFB"
         speed={50}
@@ -36,7 +42,23 @@ export default function ClientLogos() {
             </div>
           ))}
         </div>
-      </Marquee>
+      </Marquee> */}
+      <div className="flex flex-wrap gap-4 justify-center items-center">
+        {clients.map((client) => (
+          <div
+            key={client.image}
+            className="text-2xl font-medium text-gray-400 hover:text-gray-600 transition-colors duration-300 w-[200px] flex items-center justify-center font-raleway"
+          >
+            <Image
+              src={client.image}
+              alt={client.image}
+              width={200}
+              height={200}
+              className="w-full h-full object-contain border border-gray-200 rounded-md hover:scale-105 transition-all duration-300"
+            />
+          </div>
+        ))}
+      </div>
     </section>
-  )
-} 
+  );
+}
